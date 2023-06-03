ef34929489d7e2dcc30603aa3e80a995b7a0e04e
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2023-06-03 15:32:46 +0200

    update schema

--- src/pages/api/getWasteDropOffPoints.ts
+++ src/pages/api/getWasteDropOffPoints.ts
@@ -1,7 +1,17 @@
 "use client"
-export default function handler(req, res) {
-    if (req.method === 'GET') {
+import wasteDropOffPointSchema from "@/backend/models/wasteDropOffPointSchema";
+import {dbConnect, testConnection} from "../../db/db"
+
+// var conn = dbConnect();
+// conn.
+
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+    await dbConnect();
       res.status(200).json({ text: 'Hello World' });
+      const newLoc = await wasteDropOffPointSchema.create({ name: 'Ekomat', lng: 1, lat: 2 });
+      await newLoc.save();
     } else {
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);

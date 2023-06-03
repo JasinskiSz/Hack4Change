24b0640cec7adc5fe3bf1df3ecdceb43f56c2c1c
Jozef Piecyk <jpiecyk@evojam.com>
2023-06-03 19:22:13 +0200

    split map components with autocomplete and useDebounce to give some structure to project

---
+++ src/app/components/map/LocationSearchAutocomplete.tsx
@@ -0,0 +1,97 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { Autocomplete, TextField, CircularProgress } from '@mui/material';
+import { RawResult } from 'leaflet-geosearch/dist/providers/openCageProvider';
+import { SearchResult } from 'leaflet-geosearch/dist/providers/provider';
+import axios from 'axios';
+import { useDebounce } from '@/app/utils/useDebounce';
+import { LatLng } from 'leaflet';
+
+export const LocationSearchAutocomplete = (props: { onSelectedLocation: (coordinates: LatLng) => void }) => {
+  const [options, setOptions] = useState<SearchResult<RawResult>[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState('');
+  const debouncesSearchValue = useDebounce(searchValue, 500);
+
+  console.log({ options });
+  const fetchOptions = async (query: string) => {
+    setLoading(true);
+    const response = await axios.get<SearchResult<RawResult>[]>(`/api/locations?query=${query}`);
+    setLoading(false);
+    setOptions(response.data);
+  };
+
+  useEffect(() => {
+    if (debouncesSearchValue) {
+      fetchOptions(debouncesSearchValue);
+    }
+  }, [debouncesSearchValue]);
+
+  const autocompleteOptions = options.map((option) => {
+    return {
+      label: option.formatted,
+      value: option,
+    };
+  });
+
+  return (
+    <Asynchronous
+      options={autocompleteOptions}
+      loading={loading}
+      handleOptionSelected={(value) => {
+        props.onSelectedLocation(value.geometry);
+      }}
+      handleValueChange={setSearchValue}
+    />
+  );
+};
+
+function Asynchronous(props: {
+  options: { label: string; value: unknown }[];
+  handleValueChange: (value: string) => void;
+  handleOptionSelected: (value: any) => void;
+  loading: boolean;
+}) {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      sx={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      onInputChange={(_event, value) => {
+        props.handleValueChange(value);
+      }}
+      filterOptions={(x) => x}
+      onChange={(_event, option) => {
+        console.log({ option });
+        props.handleOptionSelected(option?.value);
+      }}
+      isOptionEqualToValue={(option, value) => option.label === value.label}
+      getOptionLabel={(option) => option.label}
+      options={props.options}
+      loading={props.loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Adres"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {props.loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
--- src/app/components/map/MapComponent.tsx
+++ src/app/components/map/MapComponent.tsx
@@ -1,119 +1,13 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import { Icon } from 'leaflet';
 import { LatLng, LatLngExpression, latLng } from 'leaflet';
-import 'node_modules/leaflet-geosearch/dist/geosearch.css';
-import { Autocomplete, TextField, CircularProgress } from '@mui/material';
-import { RawResult } from 'leaflet-geosearch/dist/providers/openCageProvider';
-import { SearchResult } from 'leaflet-geosearch/dist/providers/provider';
-import axios from 'axios';
+import { LocationSearchAutocomplete } from './LocationSearchAutocomplete';
 
 const CustomIcon = new Icon({ iconUrl: markerIconPng.src, iconSize: [25, 41], iconAnchor: [12, 41] });
-
-function Asynchronous(props: {
-  options: { label: string; value: unknown }[];
-  handleValueChange: (value: string) => void;
-  handleOptionSelected: (value: any) => void;
-  loading: boolean;
-}) {
-  const [open, setOpen] = React.useState(false);
-  return (
-    <Autocomplete
-      id="asynchronous-demo"
-      sx={{ width: 300 }}
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      onInputChange={(_event, value) => {
-        props.handleValueChange(value);
-      }}
-      filterOptions={(x) => x}
-      onChange={(_event, option) => {
-        props.handleOptionSelected(option?.value);
-      }}
-      isOptionEqualToValue={(option, value) => option.label === value.label}
-      getOptionLabel={(option) => option.label}
-      options={props.options}
-      loading={props.loading}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Adres"
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {props.loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
-          }}
-        />
-      )}
-    />
-  );
-}
-
-function useDebounce<T>(value: T, delay?: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
-}
-
-const LocationSearch = () => {
-  const [options, setOptions] = useState<SearchResult<RawResult>[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [searchValue, setSearchValue] = useState('');
-  const debouncesSearchValue = useDebounce(searchValue, 500);
-
-  const fetchOptions = async (query: string) => {
-    setLoading(true);
-    // const response = await provider.search({ query });
-    const response = await axios.get<SearchResult<RawResult>[]>(`/api/locations?query=${query}`);
-    setLoading(false);
-    setOptions(response.data);
-  };
-
-  useEffect(() => {
-    if (debouncesSearchValue) {
-      fetchOptions(debouncesSearchValue);
-    }
-  }, [debouncesSearchValue]);
-
-  const autocompleteOptions = options.map((option) => {
-    return {
-      label: option.formatted,
-      value: option.raw,
-    };
-  });
-
-  return (
-    <Asynchronous
-      options={autocompleteOptions}
-      loading={loading}
-      handleOptionSelected={(value) => {
-        // TODO: move map after option is selected
-        console.log('selected option', { value });
-      }}
-      handleValueChange={setSearchValue}
-    />
-  );
-};
 
 const AddPoint = () => {
   const [currentPoint, setCurrentPoint] = useState<LatLng | null>(null);
@@ -141,10 +35,14 @@ const AddPoint = () => {
 
 const GDANSK_POSITION: LatLngExpression = [54.3475, 18.645278];
 
+const handleSelectedLocation = (coordinates: LatLng) => {
+  console.log('selected coordinates', coordinates);
+};
+
 const page = () => {
   return (
     <div>
-      <LocationSearch />
+      <LocationSearchAutocomplete onSelectedLocation={handleSelectedLocation} />
       <MapContainer center={GDANSK_POSITION} zoom={20} scrollWheelZoom={false} style={{ height: '100vh' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

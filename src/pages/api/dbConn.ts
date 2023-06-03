ef2c6c1d05399c43b47fd91687a6ba06861035dc
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2023-06-03 14:52:16 +0200

    save to database from scheme

--- src/pages/api/dbConn.ts
+++ src/pages/api/dbConn.ts
@@ -1,4 +1,5 @@
 "use client"
+import wasteDropOffPointSchema from "@/backend/models/wasteDropOffPointSchema";
 import {dbConnect, testConnection} from "../../db/db"
 
 // var conn = dbConnect();
@@ -7,8 +8,10 @@ import {dbConnect, testConnection} from "../../db/db"
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
-    await testConnection();
+    await dbConnect();
       res.status(200).json({ text: 'Hello World' });
+      const newLoc = await wasteDropOffPointSchema.create({ name: 'Ekomat', lng: 1, lat: 2 });
+      await newLoc.save();
     } else {
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);

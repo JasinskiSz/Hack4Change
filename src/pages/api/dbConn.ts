98151a35f30b7c1f52afdf3960ae71c378747e92
Szymon Jasiński <szg.jasinski@gmail.com>
2023-06-03 16:43:03 +0200

    remove redundant code

--- src/pages/api/dbConn.ts
+++ src/pages/api/dbConn.ts
@@ -1,6 +1,6 @@
 "use client"
-import wasteDropOffPointSchema from "@/backend/models/wasteDropOffPointSchema";
-import {dbConnect, testConnection} from "../../db/db"
+
+import {dbConnect} from "../../db/db"
 
 // var conn = dbConnect();
 // conn.
@@ -9,9 +9,6 @@ import {dbConnect, testConnection} from "../../db/db"
 export default async function handler(req, res) {
     if (req.method === 'GET') {
       await dbConnect();
-      res.status(200).json({ text: 'Hello World' });
-      const newLoc = await wasteDropOffPointSchema.create({ name: 'Ekomat', lng: 1, lat: 2 });
-      await newLoc.save();
     } else {
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);

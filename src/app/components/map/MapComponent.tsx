69afec885093c72585fa6afd10e704d413b9c85c
Jozef Piecyk <jpiecyk@evojam.com>
2023-06-04 11:44:59 +0200

    refactor: extract fetching shops

--- src/app/components/map/MapComponent.tsx
+++ src/app/components/map/MapComponent.tsx
@@ -7,30 +7,15 @@ import { RecenterAutomatically } from './RecenterAutomatically';
 import { AddPoint } from './AddPoint';
 import { PointerIcon } from './PointerIcon';
 import { GeneratePoint } from './GeneratePoint';
-import axios from 'axios';
 
 const GDANSK_POSITION: LatLngExpression = [54.3475, 18.645278];
 
-const Page = () => {
+interface MapProps {
+  shopsWithWasteRecycling: any[];
+}
+
+const Map = ({ shopsWithWasteRecycling }: MapProps) => {
   const [selectedAddress, setSelectedAddress] = useState<LatLng | null>(null);
-  const [shopsWithWasteRecycling, setShopsWithWasteRecycling] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post('http://localhost:3000/api/ShopsWithWasteRecycling', {
-          city: 'Gdańsk',
-          shops: [],
-        });
-        setShopsWithWasteRecycling(response.data.filter((point) => point && point.lat));
-        console.log(shopsWithWasteRecycling);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleSelectedLocation = (coordinates: LatLng) => {
     setSelectedAddress(coordinates);
@@ -39,16 +24,11 @@ const Page = () => {
   return (
     <div>
       <LocationSearchAutocomplete onSelectedLocation={handleSelectedLocation} />
-      <MapContainer center={GDANSK_POSITION} zoom={20} scrollWheelZoom={false} style={{ height: '100vh' }}>
+      <MapContainer center={GDANSK_POSITION} zoom={15} scrollWheelZoom={false} style={{ height: '100vh' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={GDANSK_POSITION} icon={PointerIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
         <AddPoint lat={undefined} lng={undefined} />
         {shopsWithWasteRecycling.length > 0 && <GeneratePoint points={shopsWithWasteRecycling} />}
         {selectedAddress && <RecenterAutomatically lat={selectedAddress.lat} lng={selectedAddress.lng} />}
@@ -57,4 +37,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default Map;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,18 @@ import MapComponent from './components/map/MapComponent';
 import SearchComponent from './components/SearchComponent';
 import { Coordinates } from './types';
 import Sidebar from './components/sidebar';
+import { useShopsWithWasteRecycling } from './components/map/shopsWithWasteRecycling';
 
 const MapPage: React.FC = () => {
   const [location, setLocation] = useState<Coordinates | null>(null);
   const [query, setQuery] = useState('');
+  const { shopsWithWasteRecycling } = useShopsWithWasteRecycling();
   return (
     <div>
       <SearchComponent setQuery={setQuery} />
       <Sidebar />
       {/* <FormPage/> */}
-      <MapComponent setLocation={setLocation} />
+      <MapComponent shopsWithWasteRecycling={shopsWithWasteRecycling} />
       {location && (
         <p>
           Selected Location: {location.lat}, {location.lng}
